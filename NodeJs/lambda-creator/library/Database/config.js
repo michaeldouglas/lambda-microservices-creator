4a75d1d660c0836e69d3bd0558df172c9a74e5d9
Michael Douglas <micheldouglas010790@gmail.com>
2017-06-14 17:24:04 -0300

    Estrutura do projeto

--- NodeJs/lambda-creator/library/Database/config.js
+++ NodeJs/lambda-creator/library/Database/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  database: 'AtitudeMicroservices',
-  username: 'usrphp',
-  password: 'Atitude25',
-  host: process.env.NODE_ENV ? '192.168.0.240' : '54.207.33.185',
+  database: '',
+  username: '',
+  password: '',
+  host: process.env.NODE_ENV ? '' : ''
 };
